5936858b3cb7a496be7a1854926df1acf2c8a65d
Shantha Kumar <mailtoshan265@gmail.com>
2023-12-09 14:50:36 +0530

    Added book and available methods

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ import pandas as pd
 df = pd.read_csv('hotels.csv')
 
 
+# If You want to read data in a specific format
+# df = pd.read_csv('hotels.csv', dtype={'id':str)
+
+
 class Hotel:
     def __init__(self, hotel_id):
-        pass
+        self.hotel_id = hotel_id
 
     def available(self):
-        pass
+        # Checks the availability of a hotel
+        availability = df.loc[df['id'] == self.hotel_id, 'available'].squeeze()
+        print(availability)
+        if availability == 'yes':
+            return True
+        else:
+            return False
 
     def book(self):
+        # Once booked changes the availability to no
+        df.loc[df['id'] == self.hotel_id, 'available'] = "no"
+        df.to_csv('hotels.csv', index=False)
         pass
 
 
@@ -23,12 +36,13 @@ class ReservationTicket:
 
 
 print(df)
-hotel_id = int(input("Enter the hotel_id of the hotel you want to book "))
+hotel_id = int(input("Enter the id of the hotel you want to book "))
 hotel = Hotel(hotel_id)
+
 if hotel.available():
     hotel.book()
     name = input("Enter Your Name ")
-    ticket = ReservationTicket(name,hotel)
+    ticket = ReservationTicket(name, hotel)
     print(ticket.generate())
 else:
-    print("THe hotel is not available ")
+    print("The hotel is not available ")
