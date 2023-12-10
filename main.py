d96e77088606795908885badc7a349ea183f5f1b
Shantha Kumar <mailtoshan265@gmail.com>
2023-12-10 11:07:32 +0530

    Added Credit Card Class...

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('hotels.csv')
 
+df_card = pd.read_csv('cards.csv', dtype=str).to_dict(orient='records')
+
 
 # If You want to read data in a specific format
 # df = pd.read_csv('hotels.csv', dtype={'id':str})
@@ -15,7 +17,6 @@ class Hotel:
     def available(self):
         # Checks the availability of a hotel
         availability = df.loc[df['id'] == self.hotel_id, 'available'].squeeze()
-        print(availability)
         if availability == 'yes':
             return True
         else:
@@ -42,14 +43,32 @@ class ReservationTicket:
         return content
 
 
+class Card():
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, cvc, name):
+        card_data = {'number': self.number, 'expiration': expiration,
+                     'cvc': cvc, 'holder': name}
+        if card_data in df_card:
+            return True
+        else:
+            return False
+
+
 print(df)
 hotel_id = int(input("Enter the id of the hotel you want to book "))
 hotel = Hotel(hotel_id)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter Your Name ")
-    ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(ticket.generate())
+    card = Card('5678901234567890')
+    if card.validate(expiration='12/28', cvc='456', name='JANE SMITH'):
+        hotel.book()
+        name = input("Enter Your Name ")
+        ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+        print(ticket.generate())
+    else:
+        print("card not validated")
+
 else:
     print("The hotel is not available ")
